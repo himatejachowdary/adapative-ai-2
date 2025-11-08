041dfa809c6fc6d2d3f4588b88da6f8e184e4b08
Hima Teja Cherukumalli <himatejacherukumalli0@gmail.com>
2025-11-08 07:11:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8492299266-8b6ea",
+  "appId": "1:343893434233:web:618fd8398c2628395859a8",
+  "apiKey": "AIzaSyAzvgCzNOPJdWycHOt6VnbnOAjDXaoDvBo",
+  "authDomain": "studio-8492299266-8b6ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "343893434233"
+};
